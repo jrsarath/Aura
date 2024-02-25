e74aaae8416b2aa1e4fc125205fee6e37a1ba1e6
jrsarath <jrsarath@outlook.com>
2024-02-25 15:59:41 +0530

    working loop

--- src/main.py
+++ src/main.py
@@ -1,24 +1,24 @@
+#!/usr/bin/env python
 from dotenv import load_dotenv
 from ai import get_response
-from speech import speak
+from speech import speak, listen_for_hotword, speech_recogniztion
 
 # Load env variables
 load_dotenv()
 
-def conversation_loop():
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() == 'exit':
-            print("Exiting the conversation.")
-            break
+def conversation_loop(user_text):
+    response = get_response(user_text)
+    print("Aura: " + response)
+    speak(response) 
+    return
         
-        response = get_response(user_input)
-        speak(response)
-        print("AI: " + response)
 
 def main():
-    print("Starting conversation...")
-    conversation_loop()
+    while True:
+        if listen_for_hotword():
+            print('Starting recognizion...')
+            transcript = speech_recogniztion()
+            conversation_loop(transcript)
 
 if __name__ == "__main__":
     main()
--- src/speech.py
+++ src/speech.py
@@ -2,66 +2,69 @@ import os
 import time
 import wave
 import struct
-import whisper
 import pyaudio
 import tempfile
 import pvporcupine
 from styletts2 import tts
 from pydub import AudioSegment
-from pydub.playback import play
+from pydub.playback import _play_with_ffplay
 from dotenv import load_dotenv
+import speech_recognition as sr
+
 
 # Load env variables
 load_dotenv()
-# Initialize hotword listener
-porcupine = None
+# PyAudio
 pa = pyaudio.PyAudio()
+# Initialize hotword listener
+env_variable_value = os.getenv("PICOVOICE_API_KEY")
+porcupine = pvporcupine.create(access_key=env_variable_value, keywords=['picovoice'])
 recorder = None
 # Initialize TTS model
 TTS = tts.StyleTTS2()
-# Load Whisper model
-model = whisper.load_model(name="medium", download_root="lib")
 
 #  convert text to speech & play
 def speak(text, output_file=f'{tempfile.gettempdir()}/output-{time.time()}.wav'):
     TTS.inference(text, output_wav_file=output_file)
     audio = AudioSegment.from_wav(output_file)
-    play(audio)
+    _play_with_ffplay(audio)
     os.remove(output_file)
-    return; 
-
-def init_hotword_listener():
-    env_variable_value = os.getenv("PICOVOICE_API_KEY")
-    global porcupine
-    porcupine = pvporcupine.create(
-        access_key=env_variable_value,
-        keywords=['picovoice'])
-    global recorder
-    recorder = pa.open(
-        rate=porcupine.sample_rate,
-        channels=1,
-        format=pyaudio.paInt16,
-        input=True,
-        frames_per_buffer=porcupine.frame_length)
     return
-    
     
 def listen_for_hotword():
     try:
         print("Listening for hotword... Press Ctrl+C to exit")
+        recorder = pa.open(
+                    rate=porcupine.sample_rate,
+                    channels=1,
+                    format=pyaudio.paInt16,
+                    input=True,
+                    frames_per_buffer=porcupine.frame_length)
         while True:
             pcm = recorder.read(porcupine.frame_length)
             pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
             result = porcupine.process(pcm)
             if result >= 0:
                 print("Hotword detected!")
+                return True
     except KeyboardInterrupt:
         print("Stopping...")
-    finally:
-        recorder.close()
-        pa.terminate()
-        porcupine.delete()
+    # finally:
+        # recorder.close()
+        # pa.terminate()
+        # porcupine.delete()
+
+def speech_recogniztion():
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+    print("Transcribing...")
+    transcript = r.recognize_whisper(audio, model="medium", translate=True)
+    print("Transcript:", transcript)
+    return transcript
 
 
 if __name__ == "__main__":
-    speak("Hi, how are you")
+    listen_for_hotword()
